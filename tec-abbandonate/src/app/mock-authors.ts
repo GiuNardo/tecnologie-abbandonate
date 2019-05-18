e8d3eac039db8af2f4b92d6e820818aae201693e
Niccolò Carlo Bellucco <nico.bellucco@gmail.com>
2019-05-18 12:50:39 +0200

    aggiornamento immagini e anteprime

--- tec-abbandonate/src/app/mock-authors.ts
+++ tec-abbandonate/src/app/mock-authors.ts
@@ -1,16 +1,83 @@
 import { Author } from './author';
 
 export const AUTHORS: Author[] =   [
-    /*
     {
         id: 1,
+        name: "Sony",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 2,
+        name: "Kodak",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 3,
+        name: "JVC",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 4,
+        name: "David Paul",
+        surname: "Greg",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 5,
         name: "James",
-        surname: "Noone",
-        dataOfBirth: "08/08/1996",
-        description: "blabla"
-    },
-    {
-
+        surname: "Russel",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 6,
+        name: "Thomas",
+        surname: "Edison",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 7,
+        name: "RCA Victor",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 8,
+        name: "AEG",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 9,
+        name: "Philips",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 10,
+        name: "Texas Instruments",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 11,
+        name: "Claude",
+        surname: "Chappe",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 12,
+        name: "Siemens",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
+    }, {
+        id: 15,
+        name: "Psion",
+        surname: "",
+        dataOfBirth: "",
+        description: ""
     }
-    */
 ];
