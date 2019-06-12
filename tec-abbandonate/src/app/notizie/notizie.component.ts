b00cb763cf7c97e99ad6a53859c19bc75a7fb774
GiuNardo <giu.nardo1@gmail.com>
2019-06-12 22:36:35 +0200

    corretto cacca nelle notizie

--- tec-abbandonate/src/app/notizie/notizie.component.ts
+++ tec-abbandonate/src/app/notizie/notizie.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DATA } from '../mock-data';
-import { element } from '@angular/core/src/render3';
+import { element, elementStyleProp } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-notizie',
@@ -28,17 +28,16 @@ export class NotizieComponent implements OnInit {
     document.getElementById("input").style.width = "40%";
     document.getElementById("notizie-content").innerHTML = 
       '<style>'+
-        '#elemento-archivio { list-style:none; text-align:center; padding:20px;background-color: #ffff;}' +
         '.container-fluid{ padding: 50px!important; }'+
         '.cell-title{  color: #f8b500; font-size: 1.2em; letter-spacing: 1px; font-weight: 700;}' +
-        '.article{height: 200px; margin-top:20px; border: 1px solid grey; overflow: scroll;}' +
+        '.article{height: 150px; margin-top:20px; border: 1px solid grey; overflow: scroll; text-align:center; padding:20px;background-color: #ffff;}' +
+        '.inner-article{margin-top:10px;}'+
       '</style>';
       
     this.data.forEach(d => {
+      
       if(d.name.toLocaleLowerCase().includes(this.tec.toLocaleLowerCase())){
-        
-        document.getElementById("notizie-content").innerHTML += '<li id="elemento-archivio"></li> <br/>';
-
+    
         var superNode = document.createElement('div');
         superNode.className = "article";
         superNode.innerHTML = 'Articoli riguardanti: <span class="cell-title"> '+ d.name +'</span>';
@@ -46,17 +45,18 @@ export class NotizieComponent implements OnInit {
          d.notizie.forEach(n => {
           var node = document.createElement('div');
           node.className = "inner-article";
-          node.innerHTML = '<b>'+ n.sito+' </b> <a href="'+ n.link + '"> ' + n.name + '</a>';
+          node.innerHTML = '<b style="margin-right:10px;">'+ n.sito+' </b> <a href="'+ n.link + '"> ' + n.name + '</a>';
           superNode.appendChild(node);
            
         });
 
-        document.getElementById("elemento-archivio").appendChild(superNode);
-
+        document.getElementById("notizie-content").appendChild(superNode);
+        
       };
          
     })
     
   }
+  
 
 }
